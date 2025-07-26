4086d6d743442ae34452fd10fb045d625f860632
Campete17 <victormartinizquierdo@gmail.com>
2025-07-26 23:56:50 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import MenuSemana from "../ components/MenuSemana";
+import MenuSemana from "../// esto es para forzar el redeplieguecomponents/MenuSemana";
 
 export default function Home() {
   return (
